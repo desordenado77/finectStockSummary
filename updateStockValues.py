eb209c414352ab446990897db4787e004aeeb05c
gorka.garcia <gorka.garcia@caviumnetworks.com>
2017-08-07 16:37:32 +0200

    made it slightly more verbose

--- updateStockValues.py
+++ updateStockValues.py
@@ -18,6 +18,8 @@ fileList = ["stockValues.py", "updateStockValues.py" ]
 
 for element in fileList:    
     try:
+        print "attempting to update " + element
+        
         r = requests.get(project_url+element)
         try:
             shutil.move(path+element, path+"old_"+element)
@@ -27,6 +29,7 @@ for element in fileList:
         with open(path + element, "w") as data_file:    
             data_file.write(r.text)
             
+        print element + " Updated"
     except Exception,e: 
         print "Error found. Unable to update " + element
         print str(e)
