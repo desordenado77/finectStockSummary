f2f3cc5ee3f94228df3416e3e47db06d1186a8ac
gorka.garcia <gorka.garcia@caviumnetworks.com>
2017-08-07 16:30:59 +0200

    allow updater script to update itself

--- updateStockValues.py
+++ updateStockValues.py
@@ -14,21 +14,19 @@ path = "./"
 if sys.platform == "linux4":
     path = "/storage/emulated/0/qpython/scripts/"
 
-try:
-    r = requests.get(project_url+stock_values_file_name)
+fileList = ["stockValues.py", "updateStockValues.py" ]  
+
+for element in fileList:    
     try:
-        shutil.move(path+stock_values_file_name, path+old_stock_values_file_name)
+        r = requests.get(project_url+element)
+        try:
+            shutil.move(path+element, path+"old_"+element)
+        except Exception,e: 
+            print str(e)        
+            
+        with open(path + element, "w") as data_file:    
+            data_file.write(r.text)
+            
     except Exception,e: 
-        print str(e)        
-        
-    with open(path + stock_values_file_name, "w") as data_file:    
-        data_file.write(r.text)
-        
-#    r = requests.get(project_url+updater_file_name)
-    
-#    shutil.move(path+updater_file_name, path+old_updater_file_name)
-#    with open(path + updater_file_name) as data_file:    
-#        data_file.write(r.text)        
-except Exception,e: 
-    print "Error found. Unable to update"
-    print str(e)
+        print "Error found. Unable to update " + element
+        print str(e)
